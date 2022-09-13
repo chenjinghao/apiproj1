c8e40f6d772763c349e6d3d49f9ad9fa35e372e7
Beatrice Yap <beatriceyapsm@gmail.com>
2022-09-13 12:02:22 +0800

    __dirname update

--- backend/authdemo.js
+++ backend/authdemo.js
@@ -21,7 +21,7 @@ app.use(
 
 // req.isAuthenticated is provided from the auth router
 app.get('/', (request, response) => {
-  response.sendFile('/frontend/index.html');
+  response.sendFile(__dirname+'/frontend/index.html');
   //response.send(request.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 });
 
