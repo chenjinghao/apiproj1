53831e644343c9c3f2d086240ca500f99ce3ef10
Nithin <nithin.a7@gmail.com>
2022-09-14 00:13:56 +0800

    Add Monthly Contribution page

---
+++ frontend/scriptmonthlycont.js
@@ -0,0 +1,44 @@
+//calculate Monthly Contribution
+
+var val0 = document.getElementById('val0');
+var val1 = document.getElementById('val1');
+var val2 = document.getElementById('val2');
+var val3 = document.getElementById('val3');
+var val4 = document.getElementById('val4');
+
+
+val0.addEventListener("input", sum);
+val1.addEventListener("input", sum);
+val2.addEventListener("input", sum);
+val3.addEventListener("input", sum);
+val4.addEventListener("input", sum);
+
+function sum() {
+var myIncome = parseFloat(val0.value) || 0;
+var one = parseFloat(val1.value) || 0;
+var two = parseFloat(val2.value) || 0;
+var three = parseFloat(val3.value) || 0;
+var four = parseFloat(val4.value) || 0;
+
+var balance = myIncome-(one+two+three+four) ||0;
+
+others.innerHTML = "$ "+balance;
+
+prcnt1.innerHTML = (one/myIncome).toFixed(2);
+prcnt2.innerHTML = (two/myIncome).toFixed(2);
+prcnt3.innerHTML = (three/myIncome).toFixed(2);
+prcnt4.innerHTML = (four/myIncome).toFixed(2);
+prcnt5.innerHTML = (balance/myIncome).toFixed(2);
+
+}
+
+//clear monthly contribution form
+function myFunction() {
+    document.getElementById("myForm").reset();
+    others.innerHTML = "";
+    prcnt1.innerHTML = "";
+    prcnt2.innerHTML = "";
+    prcnt3.innerHTML = "";
+    prcnt4.innerHTML = "";
+    prcnt5.innerHTML = "";
+}
