13f5aaf839904598fc3de577ba1a2747e3a71d48
Nithin <nithin.a7@gmail.com>
2022-09-16 22:01:37 +0800

    updates to database.js

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql');
 require('dotenv').config();
-// var q = require('q');
-// var d = q.defer();
 
 // setup database connection    
 var properties = {
@@ -18,42 +16,6 @@ var mysqlConnection = mysql.createConnection(properties);
 mysqlConnection.connect((errors) => {
   if (errors) console.log("Error occurred while connecting to MySQL server");
   else console.log("Connected to MySQL successfully!");
-  // if (errors) {console.log('Not connected '.red, errors.toString().red, ' RETRYING...'.blue);
-  // d.reject();}
-  // else {console.log('Connected to Mysql. Exporting..'.blue);
-  // d.resolve(mysqlConnection);}
-  // return d.promise;
 });
 
 module.exports = { mysqlConnection };
-
-
-
-// ********Nithin's Testing********
-// 'use strict';
-// require('dotenv').config();
-// var mysql = require('mysql');
-// var q = require('q');
-// var mysqlConnection = {};
-
-// var properties = {
-//   password: process.env.DBPASSWD,
-//   user: process.env.DBUSER,
-//   database: process.env.DBNAME,
-//   host: process.env.DBHOST,
-//   port: process.env.DBPORT
-// };
-
-// mysqlConnection.connect = function () {
-//   var d = q.defer();
-//   mysqlConnection.connection = mysql.createConnection(properties);
-
-//   mysqlConnection.connection.connect((errors) => {
-//     if (errors){ console.log('Not connected '.red, errors.toString().red, ' RETRYING...'.blue); d.reject();}
-//     else {console.log('Connected to Mysql. Exporting..'.blue);
-//       d.resolve(mysqlConnection.connection);
-//     }});
-//   return d.promise;
-// };
-
-// module.exports = mysqlConnection;
