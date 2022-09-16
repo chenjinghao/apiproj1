369435dd6f7e209998aba7b77dc5b99fc788959a
Beatrice Yap <beatriceyapsm@gmail.com>
2022-09-16 17:38:52 +0800

    Pull user and asset by id /x

--- backend/api.js
+++ backend/api.js
@@ -12,4 +12,29 @@ router.get("/user/all", (request, response) => {
     }
   });
 });
+router.get("/user/:userID", (request, response) => {
+  
+  mysqlConnection.query(`SELECT * FROM users where GoogleID = ${request.params.userID}`, (errors, results) => {
+    if (errors) {
+      console.log(errors);
+      response.status(500).send("Some error occurred at the backend.");
+    } else {
+      response.status(200).send(results);
+    }
+  })
+
+});
+router.get("/asset/:userID", (request, response) => {
+  
+  mysqlConnection.query(`SELECT * FROM Assets where GoogleID = ${request.params.userID}`, (errors, results) => {
+    if (errors) {
+      console.log(errors);
+      response.status(500).send("Some error occurred at the backend.");
+    } else {
+      response.status(200).send(results);
+    }
+  })
+
+});
+
 module.exports = { router };
--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const api = require("./api");
+const body_parser = require("body-parser");
+const cors = require("cors");
 
 let app=express();
-app.use(express.json());
+//app.use(express.json());
 
+app.use(cors());
+app.use(body_parser.json());
 app.use(api.router);//ask app to use router
 app.listen(3000, (error)=> {
     if (error) {
