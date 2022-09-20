fb7bb888fca44cf3e613e9c141bcae79452d0121
Beatrice Yap <beatriceyapsm@gmail.com>
2022-09-20 18:00:08 +0800

    commenting out apis not used

--- backend/api.js
+++ backend/api.js
@@ -83,17 +83,17 @@ router.get('/assets', (req, res, next) => {
 });
 
 // 2022-09-19 BEATRICE: Get userdetails by emailID
-router.get("/getuser/:emailID", (request, response) => {
-  mysqlConnection.query(`select * from users where Email="${request.params.emailID}"`, (errors, results) => {
-    if (errors) {
-      console.log(errors);
-      console.trace('fatal error: ' + err.message);
-      response.status(500).send("Some error occurred at the backend.");
-    } else {
-      response.status(200).send(results);
-    }
-  });
-});
+//router.get("/getuser/:emailID", (request, response) => {
+//  mysqlConnection.query(`select * from users where Email="${request.params.emailID}"`, (errors, results) => {
+//    if (errors) {
+//      console.log(errors);
+//      console.trace('fatal error: ' + err.message);
+//      response.status(500).send("Some error occurred at the backend.");
+//    } else {
+//      response.status(200).send(results);
+//    }
+//  });
+//});
 
 //Nithin: Authentication Trial (sample based on FirstName -> Query in the form: {{url}}/FirstName
 // router.get("/:users", async (req, res) => {
@@ -217,6 +217,51 @@ router.put("/update/downpayment", async (req, res) => {
   });
 });
 
+//Beatrice: for savehouse
+router.put("/updatehouse", async (req, res) => {
+  const data = {
+    //FirstName: req.body.FirstName,
+    // LastName: req.body.LastName,
+    // Email: req.body.Email,
+    //DownPaymentAllocated: req.body.DownPaymentAllocated,
+    GoalAmount: req.body.GoalAmount,
+    PurchaseDate: req.body.PurchaseDate,
+    KeyCollectionDate: req.body.KeyCollectionDate,
+    DownPaymentRequired: req.body.DownPaymentRequired,
+    MonthstoGoal: req.body.MonthstoGoal,
+    MonthlyContribution: req.body.MonthlyContribution,
+    // Income: req.body.Income,
+    // PersonalSavings: req.body.PersonalSavings,
+    // Investment: req.body.Investment,
+    // Housing: req.body.Housing,
+    // Insurance: req.body.Insurance,
+    // Others: req.body.Others,
+    // Mobile: req.body.Mobile,
+    // Transport: req.body.Transport,
+    // Food: req.body.Food,
+    // GoogleID: req.body.GoogleID
+    Email: req.body.Email
+  }
+  // const query = "UPDATE users SET FirstName = ?, LastName = ?, Email = ?, DownPaymentAllocated = ?, GoalAmount = ?, PurchaseDate = ?, KeyCollectionDate = ?, DownPaymentRequired = ?, MonthstoGoal = ?, MonthlyContribution: ?, Income = ?, PersonalSavings = ?, Investment = ?, Housing = ?, Insurance = ?, Others = ?, Mobile = ?, Transport = ?, Food = ? WHERE GoogleID = ?";
+  const query = `UPDATE users SET
+  GoalAmount = ?,
+  PurchaseDate = ?,
+  KeyCollectionDate = ?,
+  DownPaymentRequired = ?,
+  MonthstoGoal = ?,
+  MonthlyContribution = ?
+  WHERE Email = ?`;
+
+  mysqlConnection.query(query, Object.values(data), (error) => {
+    if (error) {
+      res.json({ status: "failure", reason: error.code });
+      console.log(data);
+    } else {
+      res.json({ status: "success", data: data });
+    }
+  });
+});
+
 //Nithin: Update Monthly Contribution
 router.put("/update/contribution", async (req, res) => {
   const data = {
@@ -257,28 +302,28 @@ router.put("/update/contribution", async (req, res) => {
 
 
 //Nithin: Updating Table
-router.put("/update", async (req, res) => {
-  const data = {
-    FirstName: req.body.FirstName,
-    DownPaymentAllocated: req.body.DownPaymentAllocated,
-    GoalAmount: req.body.GoalAmount,
-    Email: req.body.Email
-  }
-  const query = `UPDATE users SET
-  FirstName = ?,
-  DownPaymentAllocated = ?,
-  GoalAmount = ?
-  WHERE Email = ?`;
+//router.put("/update", async (req, res) => {
+//  const data = {
+//    FirstName: req.body.FirstName,
+//    DownPaymentAllocated: req.body.DownPaymentAllocated,
+//    GoalAmount: req.body.GoalAmount,
+//    Email: req.body.Email
+//  }
+//  const query = `UPDATE users SET
+//  FirstName = ?,
+//  DownPaymentAllocated = ?,
+//  GoalAmount = ?
+//  WHERE Email = ?`;
 
-  mysqlConnection.query(query, Object.values(data), (error) => {
-    if (error) {
-      res.json({ status: "failure", reason: error.code });
-      console.log(data);
-    } else {
-      res.json({ status: "success", data: data });
-    }
-  });
-});
+//  mysqlConnection.query(query, Object.values(data), (error) => {
+//    if (error) {
+//      res.json({ status: "failure", reason: error.code });
+//      console.log(data);
+//    } else {
+//      res.json({ status: "success", data: data });
+//    }
+//  });
+//});
 
 //Nithin: Add New User to login table
 router.post("/new/login", async (req, res) => {
@@ -331,137 +376,94 @@ router.post("/new/user", async (req, res) => {
   });
 });
 
-//Beatrice: for savehouse
-router.put("/updatehouse", async (req, res) => {
-  const data = {
-    //FirstName: req.body.FirstName,
-    // LastName: req.body.LastName,
-    // Email: req.body.Email,
-    //DownPaymentAllocated: req.body.DownPaymentAllocated,
-    GoalAmount: req.body.GoalAmount,
-    PurchaseDate: req.body.PurchaseDate,
-    KeyCollectionDate: req.body.KeyCollectionDate,
-    DownPaymentRequired: req.body.DownPaymentRequired,
-    MonthstoGoal: req.body.MonthstoGoal,
-    MonthlyContribution: req.body.MonthlyContribution,
-    // Income: req.body.Income,
-    // PersonalSavings: req.body.PersonalSavings,
-    // Investment: req.body.Investment,
-    // Housing: req.body.Housing,
-    // Insurance: req.body.Insurance,
-    // Others: req.body.Others,
-    // Mobile: req.body.Mobile,
-    // Transport: req.body.Transport,
-    // Food: req.body.Food,
-    // GoogleID: req.body.GoogleID
-    Email: req.body.Email
-  }
-  // const query = "UPDATE users SET FirstName = ?, LastName = ?, Email = ?, DownPaymentAllocated = ?, GoalAmount = ?, PurchaseDate = ?, KeyCollectionDate = ?, DownPaymentRequired = ?, MonthstoGoal = ?, MonthlyContribution: ?, Income = ?, PersonalSavings = ?, Investment = ?, Housing = ?, Insurance = ?, Others = ?, Mobile = ?, Transport = ?, Food = ? WHERE GoogleID = ?";
-  const query = `UPDATE users SET
-  GoalAmount = ?,
-  PurchaseDate = ?,
-  KeyCollectionDate = ?,
-  DownPaymentRequired = ?,
-  MonthstoGoal = ?,
-  MonthlyContribution = ?
-  WHERE Email = ?`;
 
-  mysqlConnection.query(query, Object.values(data), (error) => {
-    if (error) {
-      res.json({ status: "failure", reason: error.code });
-      console.log(data);
-    } else {
-      res.json({ status: "success", data: data });
-    }
-  });
-});
 
 
 //Add New User
-router.post("/user/add", (request, response) => {
-  mysqlConnection.query(`INSERT INTO users (GoogleID, FirstName, LastName, Email)
-  values ('${request.body.GoogleID}','${request.body.FirstName}', '${request.body.LastName}', '${request.body.Email}')`, (errors, results) => {
-    if (errors) {
-      console.log(errors);
-      response.status(500).send("Some error occurred at the backend.");
-    } else {
-      response.status(200).send("Created successfully!");
-    }
-  })
-});
+//router.post("/user/add", (request, response) => {
+//  mysqlConnection.query(`INSERT INTO users (GoogleID, FirstName, LastName, Email)
+//  values ('${request.body.GoogleID}','${request.body.FirstName}', '${request.body.LastName}', '${request.body.Email}')`, (errors, results) => {
+//    if (errors) {
+//      console.log(errors);
+//      response.status(500).send("Some error occurred at the backend.");
+//    } else {
+//      response.status(200).send("Created successfully!");
+//    }
+//  })
+//});
 
 //update goalamt for savehouse.html
-router.put("/goalamt/:userID", (request, response) => {
-  mysqlConnection.query(`
-  UPDATE users 
-  SET
-    GoalAmount = ${request.body.GoalAmount},
-    PurchaseDate = "${request.body.PurchaseDate}",
-    KeyCollectionDate = "${request.body.KeyCollectionDate}"
-  WHERE
-    GoogleID = ${request.params.userID}`), (errors, results) => {
-      if (errors) {
-        console.log(errors);
-        response.status(500).send("Some error occurred at the backend.");
-      } else {
-        response.status(200).send("GoalAmount Update!");
-      }
-    }
-});
+//router.put("/goalamt/:userID", (request, response) => {
+//  mysqlConnection.query(`
+//  UPDATE users 
+//  SET
+//    GoalAmount = ${request.body.GoalAmount},
+//    PurchaseDate = "${request.body.PurchaseDate}",
+//    KeyCollectionDate = "${request.body.KeyCollectionDate}"
+//  WHERE
+//    GoogleID = ${request.params.userID}`), (errors, results) => {
+//      if (errors) {
+//        console.log(errors);
+//        response.status(500).send("Some error occurred at the backend.");
+//      } else {
+//        response.status(200).send("GoalAmount Update!");
+//      }
+//    }
+//});
 
 //update income for monthlycont.html
-router.put("/income/:userID", (request, response) => {
-  mysqlConnection.query(`
-  UPDATE users 
-  SET
-    PersonalSavings = ${request.body.PersonalSavings},
-    Investment = ${request.body.Investment},
-    Housing = ${request.body.Housing},
-    Insurance = ${request.body.Insurance}
-  WHERE
-    GoogleID = ${request.params.userID}`), (errors, results) => {
-      if (errors) {
-        console.log(errors);
-        response.status(500).send("Some error occurred at the backend.");
-      } else {
-        response.status(200).send("Income Update!");
-      }
-    }
-});
+//router.put("/income/:userID", (request, response) => {
+//  mysqlConnection.query(`
+//  UPDATE users 
+//  SET
+//    PersonalSavings = ${request.body.PersonalSavings},
+//    Investment = ${request.body.Investment},
+//    Housing = ${request.body.Housing},
+//    Insurance = ${request.body.Insurance}
+//  WHERE
+//    GoogleID = ${request.params.userID}`), (errors, results) => {
+//      if (errors) {
+//        console.log(errors);
+//        response.status(500).send("Some error occurred at the backend.");
+//      } else {
+//        response.status(200).send("Income Update!");
+//      }
+//    }
+//});
 
 //Add Asset by GoogleID (addaccount.html)
-router.post("/addasset/:userID", (request, response) => {
+//router.post("/addasset/:userID", (request, response) => {
 
-  let balance = parseInt(request.body.AccountBalance);
+//  let balance = parseInt(request.body.AccountBalance);
 
-  mysqlConnection.query(`INSERT INTO assets (GoogleID, AccountName, AccountNumber, AccountBalance)
-  values ('${request.params.userID}','${request.body.AccountName}', '${request.body.AccountNumber}', '${balance}')`, (errors, results) => {
-    if (errors) {
-      console.log(errors);
-      response.status(500).send("Some error occurred at the backend.");
-    } else {
-      response.status(200).send("Created successfully!");
-    }
-  })
+//  mysqlConnection.query(`INSERT INTO assets (GoogleID, AccountName, AccountNumber, AccountBalance)
+//  values ('${request.params.userID}','${request.body.AccountName}', '${request.body.AccountNumber}', '${balance}')`, (errors, results) => {
+//    if (errors) {
+//      console.log(errors);
+//      response.status(500).send("Some error occurred at the backend.");
+//    } else {
+//      response.status(200).send("Created successfully!");
+//    }
+//  })
 
-});
+//});
 
 //update user by GoogleID (cont. addaccount.html)
-router.put("/downpayment/:userID", (request, response) => {
-  mysqlConnection.query(`
-  UPDATE users 
-  SET
-    DownPaymentAllocated = ${request.body.DownPaymentAllocated}
-  WHERE
-    GoogleID = ${request.params.userID}`), (errors, results) => {
-      if (errors) {
-        console.log(errors);
-        response.status(500).send("Some error occurred at the backend.");
-      } else {
-        response.status(200).send("downpayment Update!");
-      }
-    }
-});
+//router.put("/downpayment/:userID", (request, response) => {
+//  mysqlConnection.query(`
+//  UPDATE users 
+//  SET
+//    DownPaymentAllocated = ${request.body.DownPaymentAllocated}
+//  WHERE
+//    GoogleID = ${request.params.userID}`), (errors, results) => {
+//      if (errors) {
+//        console.log(errors);
+//        response.status(500).send("Some error occurred at the backend.");
+//      } else {
+//        response.status(200).send("downpayment Update!");
+//      }
+//    }
+//});
 
 //Landing Page (No landing page from backend. we need it to only return json)
 //router.get('/', (req, res) => {
